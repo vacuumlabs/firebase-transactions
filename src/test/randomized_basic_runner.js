325755c14609b9d96c9268931c319c8ffccfb689
Tomas Kulich <tomas.kulich@gmail.com>
2015-10-15 19:30:49 +0200

    .

--- src/test/randomized_basic_runner.js
+++ src/test/randomized_basic_runner.js
@@ -29,7 +29,7 @@ describe('randomized', function() {
 
   for (let userCount of [10, 20, 50, 100]) {
     for (let maxWait of [10, 20, 50, 100]) {
-      for (let trCount of [50, 100]) {
+      for (let trCount of [10, 20]) {
         settingses.push({userCount, maxWait, trCount, baseCredit: 100, handlerNames: handlers})
       }
     }
