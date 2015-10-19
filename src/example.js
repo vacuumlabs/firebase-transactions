aab3cf9f25cff70f0b4009af9e441ddd1042f9ae
Tomas Kulich <tomas.kulich@gmail.com>
2015-10-19 02:58:29 +0200

    more sensible default used in example.js

--- src/example.js
+++ src/example.js
@@ -1,6 +1,6 @@
-import {test} from './test/randomized_basic'
+import {test} from './test/randomized_complex'
 
-test({trCount: 100, baseCredit: 100, maxTrCredit: 100, userCount: 200, maxWait: 0})
+test({trCount: 100, baseCredit: 100, maxTrCredit: 100, userCount: 20, maxWait: 0})
 .then((res) => {
   console.log(res)
 })
