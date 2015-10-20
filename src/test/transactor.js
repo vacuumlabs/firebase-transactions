1fbfdbac36e1ef3aed7b305c0357b19b39056565
Tomas Kulich <tomas.kulich@gmail.com>
2015-10-20 10:02:14 +0200

    Split tests for transactor's change and result

--- src/test/transactor.js
+++ src/test/transactor.js
@@ -6,21 +6,30 @@ import {transactor} from '../transactor'
 import {firebaseUrl} from '../settings'
 
 describe('transactor', function() {
-  it(`updateBy, result`, () => {
+  it(`change`, () => {
     const globalFirebase = new Firebase(firebaseUrl)
     return runSandboxed(globalFirebase, (firebase) => {
       const submitTrx = getClient(firebase)
       set(firebase.child('obj'), {'val': 1})
       const incByOne = ({change}, data) => {
         return change(['obj', 'val'], (x) => x + 1)
-          .then((_) => 'hello')
       }
       transactor(firebase, {incByOne})
       return submitTrx({type: 'incByOne', data: {}})
-        .then((res) => expect(res).to.equal('hello'))
         .then((_) => read(firebase.child('obj/val')))
         .then((val) => expect(val).to.equal(2))
-    }, {prefix: 'test', deleteAfter: false})
+    }, {prefix: 'test', deleteAfter: true})
   })
-})
 
+  it(`result`, () => {
+    const globalFirebase = new Firebase(firebaseUrl)
+    return runSandboxed(globalFirebase, (firebase) => {
+      const submitTrx = getClient(firebase)
+      const returnHello = ({}, data) => 'hello'
+      transactor(firebase, {returnHello})
+      return submitTrx({type: 'returnHello', data: {}})
+        .then((res) => expect(res).to.equal('hello'))
+    }, {prefix: 'test', deleteAfter: true})
+  })
+
+})
